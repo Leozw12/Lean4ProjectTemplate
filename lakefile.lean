5623a6f884913284b09e6153717f56bdd6c8a24e
Lionzw <1539357146@qq.com>
2024-11-06 01:35:11 +0800

    chore: initialize Lean project with dependencies and toolchain

---
+++ lakefile.lean
@@ -0,0 +1,15 @@
+import Lake
+open Lake DSL
+
+package "LeanProject" where
+  -- add package configuration options here
+
+require "leanprover-community" / "mathlib" from git
+  "https://github.com/leanprover-community/mathlib4/" @ "20c73142afa995ac9c8fb80a9bb585a55ca38308"
+
+lean_lib «Datasets» where
+  -- add library configuration options here
+
+@[default_target]
+lean_exe "leanproject" where
+  root := `Main
